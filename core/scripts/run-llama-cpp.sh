0bdc92b64d9a36f53ec2964af9aa6752a8847926
DirectX <DirectX@users.noreply.github.com>
2026-05-08 11:02:34 +0300

    Fixes

--- core/scripts/run-llama-cpp.sh
+++ core/scripts/run-llama-cpp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-MODEL_PATH="./models/gemma-4-E4B-it-Q4_K_M.gguf"
-TEMPLATE_FILE="./models/templates/google-gemma-4-31B-it-interleaved.jinja"
+MODEL_PATH="./core/models/gemma-4-E4B-it-Q4_K_M.gguf"
+TEMPLATE_FILE="./core/models/templates/google-gemma-4-31B-it-interleaved.jinja"
 
 llama-server \
   -m "$MODEL_PATH" \
   --chat-template-file "$TEMPLATE_FILE" \
   --cache-ram 2048 \
-  -ctxcp 2 \
-  -c 8192 \
-  --n-gpu-layers 35  # Adjust based on your VRAM
+  --ctx-size 8192 \
+  --n-predict 512 \
+  --n-gpu-layers 99  # Adjust based on your VRAM
